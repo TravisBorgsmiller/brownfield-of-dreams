7b2b3bc788ecfa2c48c01890ba9977ba34bbb3a8
Alex Latham <alexlthm@gmail.com>
2020-05-06 20:59:06 -0600

    Improve readability

--- app/models/git_hub/repo.rb
+++ app/models/git_hub/repo.rb
@@ -2,16 +2,16 @@ module GitHub
   class Repo
     attr_reader :name, :url
 
-    def initialize(data)
-      @name = data[:name]
-      @url = data[:html_url]
+    def initialize(repo_info)
+      @name = repo_info[:name]
+      @url = repo_info[:html_url]
     end
 
     def self.list_recent(gh_token)
-      all_repos = GitHubService.new(gh_token).repo_info
+      all_repo_info = GitHubService.new(gh_token).repo_info
 
-      all_repos[0..4].map do |repo|
-        Repo.new(repo)
+      all_repo_info[0..4].map do |repo_info|
+        Repo.new(repo_info)
       end
     end
   end
