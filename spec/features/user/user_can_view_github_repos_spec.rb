d0f901439b28f441052f0255161f1a23ad2e4339
Alex Latham <alexlthm@gmail.com>
2020-05-06 19:54:42 -0600

    Rename #github section to #github-repos

--- spec/features/user/user_can_view_github_repos_spec.rb
+++ spec/features/user/user_can_view_github_repos_spec.rb
@@ -9,7 +9,7 @@ describe 'A registered user' do
 
     visit dashboard_path
 
-    within('#github') do
+    within('#github-repos') do
       expect(page).to have_content("#{user.first_name}'s GitHub Repos")
       expect(page).to have_link('brownfield-of-dreams', href: 'https://github.com/TravisBorgsmiller/brownfield-of-dreams')
       expect(page).to have_link('monster_shop_final', href: 'https://github.com/alex-latham/monster_shop_final')
@@ -19,13 +19,13 @@ describe 'A registered user' do
     end
   end
 
-  it 'cannot see a GitHub section if they have no GitHub token' do
+  it 'cannot see a GitHub repos section if they have no GitHub token' do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit dashboard_path
 
-    expect(page).not_to have_selector('#github')
+    expect(page).not_to have_selector('#github-repos')
   end
 
   it 'cannot see other registered users GitHub repos' do
@@ -38,7 +38,7 @@ describe 'A registered user' do
 
     visit dashboard_path
 
-    within('#github') do
+    within('#github-repos') do
       expect(page).to have_no_content("#{user1.first_name}'s GitHub Repos")
       expect(page).to have_no_link('brownfield-of-dreams', href: 'https://github.com/TravisBorgsmiller/brownfield-of-dreams')
       expect(page).to have_no_link('monster_shop_final', href: 'https://github.com/alex-latham/monster_shop_final')
