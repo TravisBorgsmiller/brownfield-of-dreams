10ebc3b66aa68110d394328b053f52bc1b101841
Alex Latham <alexlthm@gmail.com>
2020-05-07 19:05:44 -0600

    Add GH follower and following specs

---
+++ spec/models/github_following_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe GitHub::Following, type: :model do
+  it "exists" do
+    attributes = {
+      login: "bob",
+      html_url: "www.github.com/users/bob"
+    }
+
+    follower = GitHub::Following.new(attributes)
+    require 'pry'; binding.pry
+
+    expect(follower).to be_a(GitHub::Following)
+    expect(follower.login).to eq("bob")
+    expect(follower.url).to eq("www.github.com/users/bob")
+  end
+end
