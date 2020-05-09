e940a33bed98639f28ea1ed86ff118fadc941383
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-05-09 16:08:28 -0500

    add method for creating video and tutorial database objects

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,4 +2,26 @@ class Tutorial < ApplicationRecord
   has_many :videos, -> { order(position: :ASC) }, inverse_of: :tutorial
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
+
+  def self.import_from_playlist(playlist_id)
+    yt_playlist = YouTube::Playlist.import(playlist_id)
+    tutorial = Tutorial.new(
+      title: yt_playlist.title,
+      description: yt_playlist.description,
+      thumbnail: yt_playlist.thumbnail,
+      playlist_id: yt_playlist.id
+    )
+
+    yt_videos = YouTube::Video.import_from_playlist(playlist_id)
+    yt_videos.each do |yt_video|
+      Video.create(
+        title: yt_video.title,
+        description: yt_video.description,
+        thumbnail: yt_video.thumbnail,
+        video_id: yt_video.id,
+        tutorial: tutorial
+      )
+    end
+    tutorial
+  end
 end
