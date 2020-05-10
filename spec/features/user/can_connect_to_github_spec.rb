3ec342ffdd099c4e5ec595f2d884c53cfc001ead
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-05-10 17:49:01 -0500

    write testing for omniauth

--- spec/features/user/can_connect_to_github_spec.rb
+++ spec/features/user/can_connect_to_github_spec.rb
@@ -7,7 +7,7 @@ describe "As a logged in user" do
     end
 
     scenario "after I go through the oauth process I have a token and can see all my github info" do
-      user = create(:user, github_token: nil)
+      user = create(:user, gh_token: nil)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -31,30 +31,15 @@ describe "As a logged in user" do
       expect(page).to have_css("#github-followers")
       expect(page).to have_css("#github-following")
 
-      expect(page).to have_content("Github Repos")
-      expect(page).to have_content("Github Followers")
-      expect(page).to have_content("Following on Github")
+      expect(page).to have_content("Repos")
+      expect(page).to have_content("Followers")
+      expect(page).to have_content("Following")
 
       expect(current_path).to eq("/dashboard")
 
       user = User.first
 
-      expect(user.github_token.nil?).to eq(false)
-      expect(user.uid).to eq(51250305)
+      expect(user.gh_token.nil?).to eq(false)
     end
   end
 end
-
-
-
-
-
-
-
-# As a user
-# When I visit /dashboard
-# Then I should see a link that is styled like a button that says "Connect to Github"
-# And when I click on "Connect to Github"
-# Then I should go through the OAuth process
-# And I should be redirected to /dashboard
-# And I should see all of the content from the previous Github stories (repos, followers, and following)
