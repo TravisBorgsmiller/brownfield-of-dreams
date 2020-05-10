790c926511c352d131cad01e6ccbec5e306548cb
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-05-10 11:04:44 -0500

    fix migration back

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_231021) do
+ActiveRecord::Schema.define(version: 2020_05_06_202706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2020_03_27_231021) do
     t.datetime "updated_at", null: false
     t.integer "uid"
     t.string "github_token"
+    t.string "gh_token"
     t.index ["email"], name: "index_users_on_email"
   end
 
