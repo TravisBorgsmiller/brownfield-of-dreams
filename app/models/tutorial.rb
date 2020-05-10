3e8cdbbb082d38b1ffbe81d9705e0d77324d9ed9
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-05-10 18:29:12 -0500

    break tutorial method into 3 methods

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -5,23 +5,31 @@ class Tutorial < ApplicationRecord
 
   def self.import_from_playlist(playlist_id)
     yt_playlist = YouTube::Playlist.import(playlist_id)
-    tutorial = Tutorial.new(
-      title: yt_playlist.title,
-      description: yt_playlist.description,
-      thumbnail: yt_playlist.thumbnail,
-      playlist_id: yt_playlist.id
-    )
+    @tutorial = create_playlist(yt_playlist)
 
     yt_videos = YouTube::Video.import_from_playlist(playlist_id)
+    create_videos(yt_videos)
+    @tutorial
+  end
+
+  def self.create_videos(yt_videos)
     yt_videos.each do |yt_video|
       Video.create(
         title: yt_video.title,
         description: yt_video.description,
         thumbnail: yt_video.thumbnail,
         video_id: yt_video.id,
-        tutorial: tutorial
+        tutorial: @tutorial
       )
     end
-    tutorial
+  end
+
+  def self.create_playlist(yt_playlist)
+    Tutorial.new(
+      title: yt_playlist.title,
+      description: yt_playlist.description,
+      thumbnail: yt_playlist.thumbnail,
+      playlist_id: yt_playlist.id
+    )
   end
 end
