58f4dd254ccaac09fa56a5af2f0439bb4ebf4c3c
Alex Latham <alexlthm@gmail.com>
2020-05-11 14:45:42 -0600

    Re-add gh_uid to users table

--- db/migrate/20200511192647_add_gh_uid_to_users.rb
+++ db/migrate/20200511192647_add_gh_uid_to_users.rb
@@ -1,5 +1,5 @@
-class AddUidToUsers < ActiveRecord::Migration[5.2]
+class AddGhUidToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :gh_uid, :integer
+    add_column :users, :gh_uid, :string
   end
 end
