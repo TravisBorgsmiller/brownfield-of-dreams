09c3e5b1eb69c77e6cff00c63ba660bfbe93cd2f
Alex Latham <alexlthm@gmail.com>
2020-05-11 14:46:28 -0600

    Add uid attribute and registered_user? method

--- app/models/git_hub/user.rb
+++ app/models/git_hub/user.rb
@@ -1,10 +1,15 @@
 module GitHub
   class User
-    attr_reader :login, :url
+    attr_reader :login, :url, :uid
 
     def initialize(attributes)
       @login = attributes[:login]
       @url = attributes[:html_url]
+      @uid = attributes[:id]
+    end
+
+    def registered_user?
+      ::User.find_by(gh_uid: @uid).present?
     end
   end
 end
