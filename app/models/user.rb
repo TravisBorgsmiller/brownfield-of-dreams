2793fde97e58bab0ca7927b446a422bb83f04f26
Alex Latham <alexlthm@gmail.com>
2020-05-11 15:01:49 -0600

    Add relationship between users and friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   has_many :user_videos, dependent: :destroy
   has_many :videos, through: :user_videos
 
+  has_many :followers, foreign_key: :follower_id , class_name: "Friendship"
+  has_many :followed, through: :followers
+  has_many :followed, foreign_key: :followed_id, class_name: "Friendship"
+  has_many :followers, through: :followed
+
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true, on: :create
   validates :first_name, presence: true
