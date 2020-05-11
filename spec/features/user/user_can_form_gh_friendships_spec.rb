6667bd0af0cff2fb4d2c173d7dfc896679361c7e
Alex Latham <alexlthm@gmail.com>
2020-05-11 14:45:25 -0600

    Update test

--- spec/features/user/user_can_form_gh_friendships_spec.rb
+++ spec/features/user/user_can_form_gh_friendships_spec.rb
@@ -2,27 +2,22 @@ require 'rails_helper'
 
 describe 'User' do
   it 'can become friends with a user if they both have accounts' do
-    user1 = create(:user, gh_token: ENV['GITHUB_TOKEN1'])
-    user2 = create(:user, gh_token: ENV['GITHUB_TOKEN2'])
+    alex = create(:user, gh_token: ENV['GITHUB_TOKEN1'], gh_uid: ENV['GITHUB_UID1'])
+    javier = create(:user, gh_token: ENV['GITHUB_TOKEN2'], gh_uid: ENV['GITHUB_UID2'])
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(alex)
 
     visit dashboard_path
 
-    within('#follower-javier-aguilar') do
+    within('#github-followers') do
       expect(page).to have_content('javier-aguilar | Add as Friend')
+      expect(page).to have_no_content('tayjames | Add as Friend')
+
     end
 
-    within('#following-javier-aguilar') do
+    within('#github-following') do
       expect(page).to have_content('javier-aguilar | Add as Friend')
-    end
-
-    within('#follower-tayjames') do
-      expect(page).to have_no_content('Add as Friend')
-    end
-
-    within('#following-tayjames') do
-      expect(page).to have_no_content('Add as Friend')
+      expect(page).to have_no_content('tayjames | Add as Friend')
     end
   end
 end
