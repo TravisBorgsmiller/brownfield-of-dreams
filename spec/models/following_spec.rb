449fec5a69b3bfda68fdc4e60867090f40b7607f
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-05-11 17:53:01 -0500

    add use cassette syntax to tests for vcr

--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe GitHub::Following, type: :model do
   it "self.list_all" do
-    list_all = GitHub::Following.list_all(ENV['GITHUB_TOKEN1'])
+    VCR.use_cassette('model_following') do
+      list_all = GitHub::Following.list_all(ENV['GITHUB_TOKEN1'])
 
-    expect(list_all).to be_a(Array)
-    expect(list_all.first).to be_a(GitHub::User)
+      expect(list_all).to be_a(Array)
+      expect(list_all.first).to be_a(GitHub::User)
+    end
   end
 end
