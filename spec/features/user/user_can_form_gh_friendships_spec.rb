b08c40c9d99ba3f0b39fe813dd3d67280f7431a7
Alex Latham <alexlthm@gmail.com>
2020-05-12 14:18:50 -0600

    Update tests with cassettes

--- spec/features/user/user_can_form_gh_friendships_spec.rb
+++ spec/features/user/user_can_form_gh_friendships_spec.rb
@@ -6,48 +6,53 @@ describe 'User' do
     @travis = create(:user, gh_token: ENV['GITHUB_TOKEN2'], gh_uid: ENV['GITHUB_UID2'])
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@alex)
-
-    visit dashboard_path
   end
 
   it 'is given a link to become friends with another user if they both have accounts and are not already friends' do
-    within('#github-followers') do
-      within('#follower-TravisBorgsmiller') do
-        expect(page).to have_button('Add as Friend')
+    VCR.use_cassette('can_form_friendships') do
+      visit dashboard_path
+      within('#github-followers') do
+        within('#follower-TravisBorgsmiller') do
+          expect(page).to have_button('Add as Friend')
+        end
+        within('#follower-tayjames') do
+          expect(page).to have_no_button('Add as Friend')
+        end
       end
-      within('#follower-tayjames') do
-        expect(page).to have_no_button('Add as Friend')
-      end
-    end
-#
-    within('#github-following') do
-      within('#following-TravisBorgsmiller') do
-        expect(page).to have_button('Add as Friend')
-      end
-      within('#following-tayjames') do
-        expect(page).to have_no_button('Add as Friend')
+
+      within('#github-following') do
+        within('#following-TravisBorgsmiller') do
+          expect(page).to have_button('Add as Friend')
+        end
+        within('#following-tayjames') do
+          expect(page).to have_no_button('Add as Friend')
+        end
       end
     end
   end
 
   it 'can click a link to add a friend' do
-    expect(@alex.friends).to eq([])
+    VCR.use_cassette('can_add_a_link') do
+      visit dashboard_path
+      expect(@alex.friends).to eq([])
 
-    within('#following-TravisBorgsmiller') do
-      click_button('Add as Friend')
+      within('#following-TravisBorgsmiller') do
+        click_button('Add as Friend')
+      end
+
+      @alex.reload
+      visit dashboard_path
+
+      within('#following-TravisBorgsmiller') do
+        expect(page).to have_no_button("Add as Friend")
+      end
+
+      expect(@alex.friends).to eq([@travis])
     end
-
-    # visit dashboard_path
-
-    within('#following-TravisBorgsmiller') do
-      expect(page).to have_no_button("Add as Friend")
-    end
-
-    @alex.reload
-    expect(@alex.friends).to eq([@travis])
   end
 
   xit 'cannot send request to add friend if there is no matching gh_uid in system' do
+    visit dashboard_path
     visit friendships_path(gh_uid: 12345) #how to post in capybara?
 
     expect(page).to have_content("Unable to find registered user with that GitHub UID")
