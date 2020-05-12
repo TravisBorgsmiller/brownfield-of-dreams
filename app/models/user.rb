5283f323d3c5b53cd47453f830efb6e1af3fa1fe
Alex Latham <alexlthm@gmail.com>
2020-05-11 20:21:15 -0600

    Add friendship relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ApplicationRecord
   has_many :user_videos, dependent: :destroy
   has_many :videos, through: :user_videos
-
-  has_many :followers, foreign_key: :follower_id , class_name: "Friendship"
-  has_many :followed, through: :followers
-  has_many :followed, foreign_key: :followed_id, class_name: "Friendship"
-  has_many :followers, through: :followed
+  has_many :friendships
+  has_many :friends, through: :friendships
 
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true, on: :create
   validates :first_name, presence: true
+
   enum role: { default: 0, admin: 1 }
+  
   has_secure_password
 end
