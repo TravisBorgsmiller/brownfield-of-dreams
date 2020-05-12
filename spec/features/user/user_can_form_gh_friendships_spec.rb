e4ea96eec4b0a47b02e0718057642f33cfbbf816
Alex Latham <alexlthm@gmail.com>
2020-05-12 13:48:00 -0600

    Final version since check-in

--- spec/features/user/user_can_form_gh_friendships_spec.rb
+++ spec/features/user/user_can_form_gh_friendships_spec.rb
@@ -9,16 +9,24 @@ describe 'User' do
 
     visit dashboard_path
   end
+
   it 'is given a link to become friends with another user if they both have accounts and are not already friends' do
     within('#github-followers') do
-      # Uncomment next line once Travis and Alex are real friends
-      # expect(page).to have_content('TravisBorgsmiller | Add as Friend')
-      expect(page).to have_no_content('tayjames | Add as Friend')
+      within('#follower-TravisBorgsmiller') do
+        expect(page).to have_button('Add as Friend')
+      end
+      within('#follower-tayjames') do
+        expect(page).to have_no_button('Add as Friend')
+      end
     end
-
+#
     within('#github-following') do
-      expect(page).to have_content('TravisBorgsmiller | Add as Friend')
-      expect(page).to have_no_content('tayjames | Add as Friend')
+      within('#following-TravisBorgsmiller') do
+        expect(page).to have_button('Add as Friend')
+      end
+      within('#following-tayjames') do
+        expect(page).to have_no_button('Add as Friend')
+      end
     end
   end
 
@@ -26,23 +34,27 @@ describe 'User' do
     expect(@alex.friends).to eq([])
 
     within('#following-TravisBorgsmiller') do
-      click_link('Add as Friend')
+      click_button('Add as Friend')
     end
 
-    # If I don't have both the following lines, the test fails -- why?
-    @alex.reload # page continues to show a link to add a friend unless this is here
-    visit dashboard_path # already have a redirect in Friendships#create tho
+    # visit dashboard_path
 
     within('#following-TravisBorgsmiller') do
-      expect(page).to have_no_content("Add as Friend")
+      expect(page).to have_no_button("Add as Friend")
     end
 
+    @alex.reload
     expect(@alex.friends).to eq([@travis])
   end
 
   xit 'cannot send request to add friend if there is no matching gh_uid in system' do
-    visit friendships_path(gh_uid: 12345)
+    visit friendships_path(gh_uid: 12345) #how to post in capybara?
 
     expect(page).to have_content("Unable to find registered user with that GitHub UID")
   end
 end
+
+
+# request test for capybara post method
+# labeled request type test
+# folder in specs/requests
