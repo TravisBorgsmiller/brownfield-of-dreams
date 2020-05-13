5a536f096c2d8d7a5309942407ee0ad8c752366b
Alex Latham <alexlthm@gmail.com>
2020-05-12 20:27:51 -0600

    Add email token to users table for confirming email address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_230123) do
+ActiveRecord::Schema.define(version: 2020_05_13_005208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2020_05_12_230123) do
     t.string "gh_token"
     t.string "gh_uid"
     t.boolean "active", default: false
+    t.string "email_token"
     t.index ["email"], name: "index_users_on_email"
   end
 
