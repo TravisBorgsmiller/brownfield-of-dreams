45265aaad080db48dd766e9e9153b54183536c5e
Alex Latham <alexlthm@gmail.com>
2020-05-13 10:03:10 -0600

    Add sad path test

--- spec/requests/user/user_can_activate_email_with_valid_token_spec.rb
+++ spec/requests/user/user_can_activate_email_with_valid_token_spec.rb
@@ -13,4 +13,19 @@ RSpec.describe "User", type: :request do
     expect(user.active).to eq(true)
     expect(user.email_token).to eq(nil)
   end
+
+  it "cannot activate email with a valid token" do
+    email_token1 = SecureRandom.urlsafe_base64.to_s
+
+    user = create(:user, email_token: SecureRandom.urlsafe_base64.to_s)
+
+    expect(user.active).to eq(false)
+
+    get "/users/#{SecureRandom.urlsafe_base64.to_s}/confirm_email"
+
+    user.reload
+
+    expect(user.active).to eq(false)
+    expect(user.email_token).to_not eq(nil)
+  end
 end
