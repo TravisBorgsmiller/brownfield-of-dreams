27e4a702f2eb621f1c3f269ef0a89b9990a1b2e8
Alex Latham <alexlthm@gmail.com>
2020-05-13 02:06:13 -0600

    Don't display classroom tutorials on show to non-current user

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,7 @@
 class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
+    four_oh_four unless tutorial.classroom == false || current_user
     @facade = TutorialFacade.new(tutorial, params[:video_id])
   end
 end
