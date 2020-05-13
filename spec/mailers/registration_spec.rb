8f1865c08a34805130c24b20b3c6ef7a5e4cae10
Alex Latham <alexlthm@gmail.com>
2020-05-12 20:42:21 -0600

    Add test for mailer

--- spec/mailers/registration_spec.rb
+++ spec/mailers/registration_spec.rb
@@ -1,5 +1,21 @@
 require "rails_helper"
 
 RSpec.describe RegistrationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "activate" do
+    it "renders the headers" do
+      user = create(:user)
+      registration = RegistrationMailer.activate(user)
+      
+      expect(registration.subject).to eq("Please activate your account")
+      expect(registration.to).to eq([user.email])
+      expect(registration.from).to eq(["noreply@shielded-falls-56694.herokuapp.com"])
+    end
+
+    it "renders the body" do
+      user = create(:user)
+      registration = RegistrationMailer.activate(user)
+
+      expect(registration.body.encoded).to match("Please activate your account")
+    end
+  end
 end
