6214c93e9ea22be6c168ed5d7e17fd387edc2483
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-05-12 19:23:08 -0500

    migration for active column on user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_213733) do
+ActiveRecord::Schema.define(version: 2020_05_12_230123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_213733) do
     t.datetime "updated_at", null: false
     t.string "gh_token"
     t.string "gh_uid"
+    t.boolean "active", default: false
     t.index ["email"], name: "index_users_on_email"
   end
 
