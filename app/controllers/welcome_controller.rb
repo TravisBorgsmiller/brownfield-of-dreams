5ee4f3da35cfda14e0fda431f7689deb6a743e4b
Alex Latham <alexlthm@gmail.com>
2020-05-13 11:17:50 -0600

    Remove comments

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,4 @@
 class WelcomeController < ApplicationController
-  # def index
-  #   if params[:tag]
-  #     tutorials = Tutorial.tagged_with(params[:tag])
-  #     @tutorials = tutorials.paginate(page: params[:page], per_page: 5)
-  #   else
-  #     @tutorials = Tutorial.all.paginate(page: params[:page], per_page: 5)
-  #   end
-  # end
-
   def index
     tutorials = (current_user ? Tutorial.all : Tutorial.where(classroom: false))
     tutorials = tutorials.tagged_with(params[:tag]) if params[:tag]
