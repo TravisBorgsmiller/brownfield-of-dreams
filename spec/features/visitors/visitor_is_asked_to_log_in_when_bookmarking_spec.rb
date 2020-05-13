1a366986062bc41722c2d4c9d8c48f655d7bb029
Alex Latham <alexlthm@gmail.com>
2020-05-13 15:09:31 -0600

    Update test for tooltip text over disabled bookmark button for visitor

--- spec/features/visitors/visitor_is_asked_to_log_in_when_bookmarking_spec.rb
+++ spec/features/visitors/visitor_is_asked_to_log_in_when_bookmarking_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 describe 'visitor visits video show page' do
-  it 'clicks on the bookmark page and is sent to the log in page' do
+  it 'hovers over a disabled bookmark button and sees a tooltip advising to log in' do
     tutorial = create(:tutorial)
     video = create(:video, tutorial_id: tutorial.id)
 
     visit tutorial_path(tutorial)
 
-    click_on 'Bookmark'
-
-    expect(current_path).to eq(login_path)
+    within('.bookmarks-btn') do
+      expect(page).to have_button('Bookmark', disabled: true, title: "You must be logged in to bookmark videos")
+    end
   end
 end
