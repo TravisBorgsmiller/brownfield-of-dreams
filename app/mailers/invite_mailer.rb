7d22d3e049f2eadc078d7c813f30c70e0a4dcab0
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-05-13 22:13:21 -0500

    fix rubocop errors

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -2,6 +2,6 @@ class InviteMailer < ApplicationMailer
   def invite_friend(email, name, sender_name)
     @sender_name = sender_name
     @friend_name = name
-    mail(to: email, subject: "Join Brownfield of Dreams!")
+    mail(to: email, subject: 'Join Brownfield of Dreams!')
   end
 end
