6f7ac681ff78530e5c567b05e4d248b5dc07407b
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-05-13 23:54:44 -0500

    add model tests for tutorial

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,4 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :thumbnail }
+  end
+  describe "methods" do
+    it "does exist" do
+      attributes = {
+          title: "Shredding gnar",
+          description: "Skiing down the slopes on a big pow day",
+          thumbnail: "https://pbs.twimg.com/profile_images/669238564543041536/F8oJKPUG_400x400.jpg"
+      }
+
+      tutorial = Tutorial.new(attributes)
+
+      expect(tutorial).to be_a Tutorial
+
+      expect(tutorial.title).to eq("Shredding gnar")
+      expect(tutorial.description).to eq("Skiing down the slopes on a big pow day")
+      expect(tutorial.thumbnail).to eq("https://pbs.twimg.com/profile_images/669238564543041536/F8oJKPUG_400x400.jpg")
+    end
+  end
 end
